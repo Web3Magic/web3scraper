fd2afc9023f00f77c7e6da2af0417d9db0ebc142
Bryan Fullen <fullenbryan@gmail.com>
2021-12-15 11:01:16 -0800

    creating the listner and the response for the url for a website

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ const cheerio = require('cheerio')
 
 const app = express()
 
+// Listing On Homepage & Response
+app.get('/', (req,res) =>{
+  res.json('Welcome to the Web3 News API')
+})
+
+//This Grabs the URL of a Website
+app.get('/news', (req,res) => {
+  axios.get()
+})
+
+//PORT Listener and Console Log out the port
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
