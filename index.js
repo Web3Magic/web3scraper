5b9bf6612988e5d6fba0fcce3b6fd3295413e864
Bryan Fullen <fullenbryan@gmail.com>
2021-12-16 12:07:41 -0800

    working with cheerio to extract a tags

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ const cheerio = require('cheerio')
 
 const app = express()
 
-// Listing On Homepage & Response
+// Listining On Homepage & Response
 app.get('/', (req,res) =>{
   res.json('Welcome to the Web3 News API')
 })
 
 //Get endpoint localhost 8000/news
 app.get('/news', (req,res) => {
-  axios.get('https://www.coindesk.com/markets/')
+  axios.get('https://www.coindesk.com/')
   //Gets the Url endpoint of a website and outputs html response
     .then((response) => {
-    const html = response.data
+      const html = response.data
   //Initilized Cheerio 
-    const $ = cheerio.load(html)
-  // Looking for any a tag that has web3 keyword
+      const $ = cheerio.load(html)
+  // Looking for any a tag that has nft keyword
+      $('a:contains("nft")')
   })
 })
 
