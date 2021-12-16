617a07b9dc81737f0b55730c87e7feb7df3c245d
Bryan Fullen <fullenbryan@gmail.com>
2021-12-16 13:10:22 -0800

    testing script on sites

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const express = require('express')
 const cheerio = require('cheerio')
 
 const app = express()
+
 // articles array 
 const articles = []
-
 
 // Listining On Homepage & Response
 app.get('/', (req,res) =>{
@@ -15,18 +15,24 @@ app.get('/', (req,res) =>{
 
 //Get endpoint localhost 8000/news
 app.get('/news', (req,res) => {
-  axios.get('https://www.coindesk.com/')
+  axios.get('https://cryptonews.com/news/')
   //Gets the Url endpoint of a website and outputs html response
     .then((response) => {
       const html = response.data
   //Initilized Cheerio 
       const $ = cheerio.load(html)
+     
   // Looking for any a tag that has nft keyword
-      $('a:contains("nft")', html).each(function () {
+      $('a:contains("NFTs")', html).each(function () {
         const title = $(this).text()
         const url = $(this).attr('href')
+        articles.push({
+          title,
+          url
+        })
       })
-  })
+    res.json(articles)
+  }).catch((err) => console.log(err))
 })
 
 //PORT Listener and Console Log out the port
